45aff56bb26e8bd9e70003595641043de606180c
lea <le6736419@gmail.com>
2025-03-16 10:33:32 +0200

    good-users-to-client

--- api/Core/DTO/UserDto.cs
+++ api/Core/DTO/UserDto.cs
@@ -1,6 +1,6 @@
 ﻿public class UserDto
 {
-    public int Id { get; set; }
+
     public string FullName { get; set; }
     public string Email { get; set; }
     public string Role { get; set; }// "Admin" / "Typist"
